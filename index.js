25a9a2b225ae07c03ed36c8f76da34bb95ac404c
adil-khursheed <adilkhursheed60@gmail.com>
2022-12-07 14:19:05 +0530

    Logic added

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
-// fetch("https://api.adviceslip.com/advice")
-// .then((response) => response.json())
-// .then((json) => console.log(json));
-
-const numberAdvice = document.getElementById("advice-id");
-const textAdvice = document.getElementById("advice");
-const adviceButton = document.getElementById("dice-btn");
 
 
-async function getAdvice() {
+
+const fetchAdvice = async () => {
     // getting response from server
-    let myResponse = await fetch("https://api.adviceslip.com/advice");
+    const res = await fetch("https://api.adviceslip.com/advice");
     // getting a JSON string from the response from the server
-    let myString = await myResponse.text();
-    // getting an object from JSON string
-    let myObject = JSON.parse(myString);
-    // getting a advice number
-    let numAdv = myObject.slip["id"];
-    // getting advice text
-    let advice = myObject.slip["advice"];
-    // set content
-    numberAdvice.innerHTML = `${numAdv}`;
-    textAdvice.innerHTML = `${advice}`;
+    const data = await res.json();
+    console.log(data);
+    document.getElementById("advice-id").innerHTML = `Advice #${data.slip.id}`;
+    document.getElementById("advice").innerHTML = `"${data.slip.advice}"`;
 }
 
-getAdvice();
-
-adviceButton.addEventListener("click", getAdvice);
+fetchAdvice();
